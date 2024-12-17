621884a0e4f9de9e5a96b3f30292fb45380d19fc
pb319 <pranaybiswas319@gmail.com>
2024-12-17 23:05:43 +0530

    1st File Added

---
+++ BigO.py
@@ -0,0 +1,12 @@
+#It is about finding a number within an array.
+a = [1,9,12,23,37,45,59]
+
+x = int(input())
+for i in range(len(a)):
+    if a[i] == x:
+        print(f"Number of iteration needed:{i}")
+
+#Binary Search
+
+while 
+len(a)/2
