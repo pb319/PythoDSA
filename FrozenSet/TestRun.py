d4609772ba96fb8b4268cc495029078f7068557b
pb319 <pranaybiswas319@gmail.com>
2025-02-06 19:29:47 +0530

    Frozen Set Added

---
+++ FrozenSet/TestRun.py
@@ -0,0 +1,10 @@
+# Set Datastructure (Not Subcriptable)
+def set_trans(lst):
+    return set(lst)
+
+
+if __name__ == "__main__":
+    given1 = [1,"a","a","a","b","c","d"]
+    out1 = set_trans(given1)
+    print(out1)
+
