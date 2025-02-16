e1a81a55a83fc30707b4cdc7c9404f925074e7a7
pb319 <pranaybiswas319@gmail.com>
2025-02-16 13:22:29 +0530

    Merge remote-tracking branch 'origin/main'

--- UnitTest/test.py
+++ UnitTest/test.py
@@ -5,8 +5,30 @@ import calc
 class TestCalc(unittest.TestCase):
 
     def test_add(self):
-        res = calc.multipy(10,5)
-        self.assertEqual(res,51)
+        # res = calc.multipy(10,5)
+        self.assertEqual(calc.add(10,5),15)
+        self.assertEqual(calc.add(-1,1),0)
+        self.assertEqual(calc.add(-1,-3),-4)
+
+    def test_subtract(self):
+            # res = calc.multipy(10,5)
+            self.assertEqual(calc.substract(10,5),5)
+            self.assertEqual(calc.substract(-1,1),-2)
+            self.assertEqual(calc.substract(1,1),0)
+
+    def test_multiply(self):
+            # res = calc.multipy(10,5)
+            self.assertEqual(calc.multipy(10,5),50)
+            self.assertEqual(calc.multipy(-1,1),-1)
+            self.assertEqual(calc.multipy(-1,-3),3)
+
+    def test_divide(self):
+            # res = calc.multipy(10,5)
+            self.assertEqual(calc.divide(10,5),2)
+            self.assertEqual(calc.divide(-1,1),-1)
+            self.assertEqual(calc.divide(-4,-2),2)
+
+
 
 if __name__ == '__main__':
     unittest.main()
